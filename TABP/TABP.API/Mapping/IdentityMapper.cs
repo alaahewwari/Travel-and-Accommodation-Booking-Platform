48ab38ee6f074ba47885e76df421726f290399d5
AlaaHewwari <alaahewwari@gmail.com>
2025-07-01 21:29:13 +0300

    TABP-2 Add mapping from LoginUserRequest to command

---
+++ TABP/TABP.API/Mapping/IdentityMapper.cs
@@ -0,0 +1,14 @@
+﻿using Riok.Mapperly.Abstractions;
+using TABP.API.Contracts.User;
+using TABP.Application.Users.Login.Commands;
+using TABP.Application.Users.Login.Common;
+
+namespace TABP.API.Mapping
+{
+    [Mapper]
+    public partial class IdentityMapper
+    {
+        public partial LoginUserCommand ToCommand(LoginUserRequest request);
+        public partial LoginUserResponse ToResponse(LoginUserResponse source);
+    }
+}
