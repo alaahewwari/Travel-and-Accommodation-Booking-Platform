c21acbad5f9f5a62bdffce503a2a47623d1abdd0
AlaaHewwari <alaahewwari@gmail.com>
2025-07-03 16:08:53 +0300

    TABP-4 Implement city mapper for Api layer

---
+++ TABP/TABP.API/Mapping/CityMapper.cs
@@ -0,0 +1,18 @@
+﻿using Riok.Mapperly.Abstractions;
+using TABP.API.Contracts.City;
+using TABP.Application.Cities.Commands.Create;
+using TABP.Application.Cities.Commands.Update;
+namespace TABP.API.Mapping
+{
+    [Mapper]
+    public partial class CityMapper
+    {
+        public partial CreateCityCommand ToCommand(CreateCityRequest request);
+        protected partial UpdateCityCommand ToCommandInternal(UpdateCityRequest request, int id);
+        public UpdateCityCommand ToCommand(UpdateCityRequest request, int id)
+        {
+            var command = ToCommandInternal(request, id);
+            return new UpdateCityCommand(id, command.Name, command.Country, command.PostOffice);
+        }
+    }
+}
