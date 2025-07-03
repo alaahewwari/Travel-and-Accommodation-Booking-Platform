1b0501aa2b9a69caf689d74eae119e43322ac693
AlaaHewwari <alaahewwari@gmail.com>
2025-07-03 14:25:17 +0300

    TABP-4 Add CQRS commands and queries for City CRUD operations

---
+++ TABP/TABP.Application/Cities/Commands/Delete/DeleteCityCommand.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+using TABP.Application.Common;
+namespace TABP.Application.Cities.Commands.Delete
+{
+    public record DeleteCityCommand(int Id) : IRequest<Result>;
+}
---
+++ TABP/TABP.Application/Cities/Commands/Delete/DeleteCityCommandHandler.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using TABP.Application.Cities.Common;
+using TABP.Application.Common;
+using TABP.Domain.Interfaces.Repositories;
+namespace TABP.Application.Cities.Commands.Delete
+{
+    public class DeleteCityCommandHandler(ICityRepository cityRepository)
+         : IRequestHandler<DeleteCityCommand, Result>
+    {
+        public async Task<Result> Handle(DeleteCityCommand request, CancellationToken cancellationToken)
+        {
+            var city = await cityRepository.GetCityByIdAsync(request.Id, cancellationToken);
+            if (city is null)
+                return Result.Failure(CityErrors.CityNotFound);
+            await cityRepository.DeleteCityAsync(city.Id, cancellationToken);
+            return Result.Success();
+        }
+    }
+}
