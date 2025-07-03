76dc9c82dc4ffb739c322fbfe3f3e28d9ada4cb4
AlaaHewwari <alaahewwari@gmail.com>
2025-07-03 14:21:24 +0300

    TABP-4 Add City entity, repository interface

---
+++ TABP/TABP.Domain/Entites/City.cs
@@ -0,0 +1,13 @@
+﻿using TABP.Domain.Common;
+namespace TABP.Domain.Entites
+{
+    public class City : SoftDeletable
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Country { get; set; } = null!;
+        public string PostOffice { get; set; } = null!;
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
