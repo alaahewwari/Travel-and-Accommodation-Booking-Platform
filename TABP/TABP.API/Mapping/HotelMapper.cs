84487ec0914196a1461c4c0ec2e91764d77af156
AlaaHewwari <alaahewwari@gmail.com>
2025-07-05 00:19:45 +0300

    TABP-5 Add hotel mapper at api layer

---
+++ TABP/TABP.API/Mapping/HotelMapper.cs
@@ -0,0 +1,25 @@
+﻿using Riok.Mapperly.Abstractions;
+using TABP.API.Contracts.Hotels;
+using TABP.Application.Hotels.Commands.Create;
+using TABP.Application.Hotels.Commands.Update;
+namespace TABP.API.Mapping
+{
+    [Mapper]
+    public partial class HotelMapper
+    {
+        public partial CreateHotelCommand ToCommand(CreateHotelRequest request);
+        protected partial UpdateHotelCommand ToCommandInternal(UpdateHotelRequest request, int id);
+        public UpdateHotelCommand ToCommand(UpdateHotelRequest request, int id)
+        {
+            var command = ToCommandInternal(request, id);
+            return new UpdateHotelCommand(id,
+                request.Name,
+                request.CityId,
+                request.OwnerId,
+                request.LocationLatitude,
+                request.LocationLongitude,
+                request.Address
+            );
+        }
+    }
+}
