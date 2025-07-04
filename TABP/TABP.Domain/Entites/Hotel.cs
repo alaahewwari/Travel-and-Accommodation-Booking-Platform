852a2e039cf94583201c74add299abf60c37b4b7
AlaaHewwari <alaahewwari@gmail.com>
2025-07-04 23:51:43 +0300

    TABP-5 Add Hotel and Owner models

---
+++ TABP/TABP.Domain/Entites/Hotel.cs
@@ -0,0 +1,18 @@
+﻿using TABP.Domain.Common;
+namespace TABP.Domain.Entites
+{
+    public class Hotel : SoftDeletable
+    {
+        public long Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string? Description { get; set; }
+        public string BriefDescription { get; set; } = null!;
+        public string Address { get; set; } = null!;
+        public byte StarRating { get; set; }
+        public double LocationLatitude { get; set; }
+        public double LocationLongitude { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        public City City { get; set; } = null!;
+    }
+}
