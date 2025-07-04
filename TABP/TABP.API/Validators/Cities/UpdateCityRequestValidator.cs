0b4635d1f81b0c6b3f6e21216dcff020f9ffb147
AlaaHewwari <alaahewwari@gmail.com>
2025-07-05 00:04:45 +0300

    TABP-5 Update city validators

--- TABP/TABP.API/Validators/Cities/UpdateCityRequestValidator.cs
+++ TABP/TABP.API/Validators/Cities/UpdateCityRequestValidator.cs
@@ -2,18 +2,15 @@
 using TABP.API.Contracts.Cities;
 namespace TABP.API.Validators.Cities
 {
-    public class CreateCityRequestValidator : AbstractValidator<CreateCityRequest>
+    public class UpdateCityRequestValidator : AbstractValidator<UpdateCityRequest>
     {
-        public CreateCityRequestValidator()
+        public UpdateCityRequestValidator()
         {
             RuleFor(x => x.Name)
-                .NotEmpty().WithMessage("City name is required.")
                 .MaximumLength(100).WithMessage("City name must not exceed 100 characters.");
             RuleFor(x => x.Country)
-                .NotEmpty().WithMessage("Country is required.")
                 .MaximumLength(100).WithMessage("Country must not exceed 100 characters.");
             RuleFor(x => x.PostOffice)
-                .NotEmpty().WithMessage("Post office is required.")
                 .Must(IsValidPostOffice).WithMessage("Post office must be exactly 5 characters long and numeric.");
         }
         private bool IsValidPostOffice(string input)
