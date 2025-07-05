216d8bdea7a46616cff8e41c815615e41f38398a
AlaaHewwari <alaahewwari@gmail.com>
2025-07-05 15:40:10 +0300

    TABP-6 Add new model for return value in city repository

--- TABP/TABP.Domain/Interfaces/Repositories/ICityRepository.cs
+++ TABP/TABP.Domain/Interfaces/Repositories/ICityRepository.cs
@@ -1,11 +1,12 @@
 ﻿using TABP.Domain.Entites;
+using TABP.Domain.Models;
 namespace TABP.Domain.Interfaces.Repositories
 {
     public interface ICityRepository
     {
         Task<City> CreateCityAsync(City city, CancellationToken cancellationToken);
         Task<City?> GetCityByIdAsync(int cityId, CancellationToken cancellationToken);
-        Task<IEnumerable<City>> GetAllCitiesAsync(CancellationToken cancellationToken);
+        Task<IEnumerable<CityWithHotelCount>> GetAllCitiesAsync(CancellationToken cancellationToken);
         Task<City?> UpdateCityAsync(City city, CancellationToken cancellationToken);
         Task<bool> DeleteCityAsync(int cityId, CancellationToken cancellationToken);
         Task<IEnumerable<City>> GetMostBookedCitiesAsync(int count, CancellationToken cancellationToken);
---
+++ TABP/TABP.Domain/Models/CityWithHotelCount.cs
@@ -0,0 +1,12 @@
+﻿namespace TABP.Domain.Models
+{
+    public record CityWithHotelCount(
+        int Id,
+        string Name,
+        string Country,
+        string PostOffice,
+        DateTime CreatedAt,
+        DateTime UpdatedAt,
+        int NumberOfHotels
+        );
+}
