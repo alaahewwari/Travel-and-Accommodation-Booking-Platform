51c2629549357a65f5c572bb3719c6a940ebf4fc
AlaaHewwari <alaahewwari@gmail.com>
2025-07-07 01:45:28 +0300

    TABP-7 Rename mappers to mapping

---
+++ TABP/TABP.API/Mapping/CityMapping.cs
@@ -0,0 +1,13 @@
+﻿using Riok.Mapperly.Abstractions;
+using TABP.API.Contracts.Cities;
+using TABP.Application.Cities.Commands.Create;
+using TABP.Application.Cities.Commands.Update;
+namespace TABP.API.Mapping
+{
+    [Mapper]
+    public static partial class CityMapping
+    {
+        public static partial CreateCityCommand ToCommand(this CreateCityRequest request);
+        public static partial UpdateCityCommand ToCommand(this UpdateCityRequest request, int id);
+    }
+}
---
+++ TABP/TABP.API/Mapping/HotelMapping.cs
@@ -0,0 +1,13 @@
+﻿using Riok.Mapperly.Abstractions;
+using TABP.API.Contracts.Hotels;
+using TABP.Application.Hotels.Commands.Create;
+using TABP.Application.Hotels.Commands.Update;
+namespace TABP.API.Mapping
+{
+    [Mapper]
+    public static partial class HotelMapping
+    {
+        public static partial CreateHotelCommand ToCommand(this CreateHotelRequest request);
+        public static partial UpdateHotelCommand ToCommand(this UpdateHotelRequest request, long id);
+    }
+}
