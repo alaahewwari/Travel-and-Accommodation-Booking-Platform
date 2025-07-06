6f1885ff0d0b4d04d64ad381a135c01f4cbaa45f
AlaaHewwari <alaahewwari@gmail.com>
2025-07-07 01:36:48 +0300

    TABP-7 Update return value for get all hotels in hotel repo

--- TABP/TABP.Domain/Interfaces/Repositories/IHotelRepository.cs
+++ TABP/TABP.Domain/Interfaces/Repositories/IHotelRepository.cs
@@ -1,11 +1,12 @@
 ﻿using TABP.Domain.Entites;
+using TABP.Domain.Models;
 namespace TABP.Domain.Interfaces.Repositories
 {
     public interface IHotelRepository
     {
         Task<Hotel> CreateHotelAsync(Hotel hotel, CancellationToken cancellationToken);
         Task<Hotel?> GetHotelByIdAsync(long hotelId, CancellationToken cancellationToken);
-        Task<IEnumerable<Hotel>> GetAllHotelsAsync(CancellationToken cancellationToken);
+        Task<IEnumerable<HotelForManagement>> GetAllHotelsAsync(CancellationToken cancellationToken);
         Task<Hotel?> UpdateHotelAsync(Hotel hotel, CancellationToken cancellationToken);
         Task<bool> DeleteHotelAsync(long hotelId, CancellationToken cancellationToken);
         Task<bool> GetHotelByLocationAsync(double latitude, double longitude, CancellationToken cancellationToken);
