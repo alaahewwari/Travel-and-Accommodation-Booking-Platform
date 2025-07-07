6cf69aa041cf6d24add487b852b7740e8e4fcd32
AlaaHewwari <alaahewwari@gmail.com>
2025-07-08 00:39:11 +0300

    TABP-10 Add owner mapping

---
+++ TABP/TABP.API/Mapping/OwnerMapping.cs
@@ -0,0 +1,13 @@
+﻿using Riok.Mapperly.Abstractions;
+using TABP.API.Contracts.Owners;
+using TABP.Application.Owners.Commands.Create;
+using TABP.Application.Owners.Commands.Update;
+namespace TABP.API.Mapping
+{
+    [Mapper]
+    public static partial class OwnerMapping
+    {
+        public static partial CreateOwnerCommand ToCommand(this CreateOwnerRequest request);
+        public static partial UpdateOwnerCommand ToCommand(this UpdateOwnerRequest request, int id);
+    }
+}
