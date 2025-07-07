7f5796a55c79449bd519fb6b17f4b98a332aea19
AlaaHewwari <alaahewwari@gmail.com>
2025-07-08 00:44:01 +0300

    TABP-11 Add discount creation validator

---
+++ TABP/TABP.API/Validators/Discounts/CreateDiscountRequestValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using TABP.API.Contracts.Discounts;
+namespace TABP.API.Validators.Discounts
+{
+    public class CreateDiscountRequestValidator:  AbstractValidator<CreateDiscountRequest>
+    {
+        public CreateDiscountRequestValidator()
+        {
+            RuleFor(x => x.Percentage)
+                .InclusiveBetween(1, 100)
+                .WithMessage("Discount percentage must be between 1 and 100.");
+            RuleFor(x => x.StartDate)
+                .LessThan(x => x.EndDate)
+                .WithMessage("Start date must be earlier than end date.");
+            RuleFor(x => x.EndDate)
+                .GreaterThan(DateTime.UtcNow)
+                .WithMessage("End date must be in the future.");
+        }
+    }
+}
