6e9da92626fee9229b91bd3eda718c97557475d3
AlaaHewwari <alaahewwari@gmail.com>
2025-07-08 00:46:57 +0300

    TABP-11 Implement and register discount repository and it's interface

---
+++ TABP/TABP.Domain/Interfaces/Repositories/IDiscountRespository.cs
@@ -0,0 +1,11 @@
+﻿using TABP.Domain.Entites;
+namespace TABP.Domain.Interfaces.Repositories
+{
+    public interface IDiscountRespository
+    {
+        Task<Discount?> GetDiscountByRoomClassAsync(long roomClassId, CancellationToken cancellationToken);
+        Task<Discount> CreateAndAssignDiscountAsync(Discount discount, RoomClass roomClass, CancellationToken cancellationToken);
+        Task<bool> DeleteDiscountAsync(int id, CancellationToken cancellationToken);
+        Task<Discount?> GetDiscountByIdAsync(int id, CancellationToken cancellationToken);
+    }
+}
