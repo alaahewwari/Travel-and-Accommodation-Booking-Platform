bdc7b0d03501fdfd06e7183551845a8e3c446bcf
AlaaHewwari <alaahewwari@gmail.com>
2025-07-08 00:11:53 +0300

    [TABP-9] Add room mapping

---
+++ TABP/TABP.API/Mapping/RoomMapping.cs
@@ -0,0 +1,13 @@
+﻿using Riok.Mapperly.Abstractions;
+using TABP.API.Contracts.Rooms;
+using TABP.Application.Rooms.Commands.Create;
+using TABP.Application.Rooms.Commands.Update;
+namespace TABP.API.Mapping
+{
+    [Mapper]
+    public static partial class RoomMapping
+    {
+        public static partial CreateRoomCommand ToCommand(this CreateRoomRequest request, long hotelId, long roomClassId);
+        public static partial UpdateRoomCommand ToCommand(this UpdateRoomRequest request, long id);
+    }
+}
