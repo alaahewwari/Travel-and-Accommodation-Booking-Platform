bcaf69e2771b608fbac2cfbc46ff0feede11c123
AlaaHewwari <alaahewwari@gmail.com>
2025-07-08 00:44:47 +0300

    TABP-11 Implement discount use cases (commands and queries)

---
+++ TABP/TABP.Application/Discounts/Mappers/DiscountMapper.cs
@@ -0,0 +1,21 @@
+﻿using Riok.Mapperly.Abstractions;
+using TABP.Application.Discounts.Commands.Create;
+using TABP.Application.Discounts.Common;
+using TABP.Domain.Entites;
+namespace TABP.Application.Discounts.Mappers
+{
+    [Mapper]
+    public static partial class DiscountMapper
+    {
+        public static Discount ToDiscountDomain(this CreateDiscountCommand command)
+        {
+            var discount = ToDicountDomainInternal(command);
+            discount.CreatedAt = DateTime.UtcNow;
+            discount.UpdatedAt = DateTime.UtcNow;
+            return discount;
+        }
+        public static partial DiscountResponse ToDiscountResponse(this Discount discount);
+        [MapperIgnoreSource(nameof(command.RoomClassId))]
+        private static partial Discount ToDicountDomainInternal(CreateDiscountCommand command);
+    }
+}
---
+++ TABP/TABP.Application/Discounts/Queries/GetByRoomClass/GetDiscountByRoomClassQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using TABP.Application.Common;
+using TABP.Application.Discounts.Common;
+using TABP.Application.Discounts.Mappers;
+using TABP.Application.RoomClassClasses.Common;
+using TABP.Domain.Interfaces.Repositories;
+namespace TABP.Application.Discounts.Queries.GetByRoomClass
+{
+    public class GetDiscountByRoomClassQueryHandler(
+        IDiscountRespository discountRespository,
+        IRoomClassRepository roomClassRepository
+        ) : IRequestHandler<GetDiscountByRoomClassQuery, Result<DiscountResponse>>
+    {
+        public async Task<Result<DiscountResponse>> Handle(GetDiscountByRoomClassQuery request, CancellationToken cancellationToken)
+        {
+            var existingRoomClass = await roomClassRepository.GetRoomClassByIdAsync(request.roomCLassId, cancellationToken);
+            if (existingRoomClass is null)
+            {
+                return Result<DiscountResponse>.Failure(RoomClassErrors.RoomClassNotFound);
+            }
+            var discount = await discountRespository.GetDiscountByRoomClassAsync(request.roomCLassId, cancellationToken);
+            var response = discount.ToDiscountResponse();
+            return Result<DiscountResponse>.Success(response);
+        }
+    }
+}
