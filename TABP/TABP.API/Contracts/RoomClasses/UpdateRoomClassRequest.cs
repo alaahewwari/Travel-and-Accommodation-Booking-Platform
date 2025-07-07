d2b35e8386dd55b69209c3d8f03916cfd3fb19b0
AlaaHewwari <alaahewwari@gmail.com>
2025-07-07 13:04:47 +0300

    TABP-8 Add roomclass contracts

---
+++ TABP/TABP.API/Contracts/RoomClasses/UpdateRoomClassRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace TABP.API.Contracts.RoomClasses
+{
+    public record UpdateRoomClassRequest(
+        string Description,
+        int AdultsCapacity,
+        int ChildrenCapacity,
+        decimal PricePerNight
+        );
+}
