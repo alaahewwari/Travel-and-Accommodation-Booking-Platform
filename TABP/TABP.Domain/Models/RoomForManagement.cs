e248ffb5d7c12c21ea15957bf87ccc2562b5e45c
AlaaHewwari <alaahewwari@gmail.com>
2025-07-07 18:23:48 +0300

    TABP-9 Add room for management model

---
+++ TABP/TABP.Domain/Models/RoomForManagement.cs
@@ -0,0 +1,12 @@
+﻿namespace TABP.Domain.Models
+{
+    public record RoomForManagement(
+        long Id,
+        string Number,
+        bool IsAvailable,
+        int AdultsCapacity,
+        int ChildrenCapacity,
+        DateTime CreatedAtUtc,
+        DateTime? ModifiedAtUtc
+    );
+}
