73320a2a5003bc66c1381f642d2a7d02ac97435e
AlaaHewwari <alaahewwari@gmail.com>
2025-07-20 18:31:40 +0300

    TABP-13 Add hotel pagination result record for response

---
+++ TABP/TABP.Application/Hotels/Common/HotelPaginationResult.cs
@@ -0,0 +1,8 @@
+﻿using TABP.Domain.Models;
+namespace TABP.Domain.Application.Common
+{
+    public record HotelPaginationResult(
+        PaginationMetadata PaginationMetadata,
+        IEnumerable<HotelSearchResultResponse> Items
+        );
+}
