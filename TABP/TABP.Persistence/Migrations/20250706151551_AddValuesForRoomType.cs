f246b23044548583a940a543d6f0a0ba4a35d17d
AlaaHewwari <alaahewwari@gmail.com>
2025-07-20 18:33:39 +0300

    TABP-13 Add migrations to apply the changes on db and seed initial data for manual test

---
+++ TABP/TABP.Persistence/Migrations/20250706151551_AddValuesForRoomType.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TABP.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddValuesForRoomType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<byte>(
+                name: "Type",
+                table: "RoomClasses",
+                type: "tinyint",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Type",
+                table: "RoomClasses",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(byte),
+                oldType: "tinyint");
+        }
+    }
+}
