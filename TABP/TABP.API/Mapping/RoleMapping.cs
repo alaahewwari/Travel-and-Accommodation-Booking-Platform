a96ef93fa60d19159ee2868b9e4e01fde604a4c8
AlaaHewwari <alaahewwari@gmail.com>
2025-07-20 17:06:54 +0300

    TABP-12 Add role mappings

---
+++ TABP/TABP.API/Mapping/RoleMapping.cs
@@ -0,0 +1,13 @@
+﻿using Riok.Mapperly.Abstractions;
+using TABP.API.Contracts.Rules;
+using TABP.Application.Roles.Commands.Create;
+using TABP.Application.Roles.Commands.Update;
+namespace TABP.API.Mapping
+{
+    [Mapper]
+    public static partial class RoleMapping
+    {
+        public static partial CreateRoleCommand ToCommand(this CreateRoleRequest request);
+        public static partial UpdateRoleCommand ToCommand(this UpdateRoleRequest request, int Id);
+    }
+}
