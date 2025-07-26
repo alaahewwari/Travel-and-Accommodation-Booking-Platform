d448cd9e112db7a58bdeeaeaaba69e81152d6359
AlaaHewwari <alaahewwari@gmail.com>
2025-07-26 03:13:12 +0300

    TABP-14 Implement pricing service to decouple from logic

---
+++ TABP/TABP.Domain/Services/Pricing/PricingService.cs
@@ -0,0 +1,15 @@
+﻿using TABP.Domain.Entities;
+using TABP.Domain.Services.Booking;
+namespace TABP.Domain.Services.Pricing
+{
+    public sealed class PricingService : IPricingService
+    {
+        public decimal CalculateTotalPrice(IEnumerable<Room> rooms, int nights)
+        {
+            if (rooms is null) throw new ArgumentNullException(nameof(rooms));
+            if (nights <= 0) throw new ArgumentException("Nights must be positive", nameof(nights));
+            var basePrice = rooms.Sum(room => room.RoomClass.PricePerNight * nights);
+            return basePrice;
+        }
+    }
+}
