a4705bc1e0ed74b904af62fdac249125bd6a0686
AlaaHewwari <alaahewwari@gmail.com>
2025-07-26 03:09:44 +0300

    TABP-14 Implement SmtpEmailService for sending emails via SMTP

---
+++ TABP/TABP.Infrastructure/Services/Email/SmtpEmailService.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Options;
+using System.Net.Mail;
+using TABP.Domain.Interfaces.Services;
+using TABP.Domain.Models.Email;
+using TABP.Infrastructure.Configurations;
+namespace TABP.Infrastructure.Services.Email
+{
+    public class SmtpEmailService(IOptionsMonitor<SmtpSettings> settings) : IEmailService
+    {
+        public async Task SendEmailAsync(EmailMessage message)
+        {
+            using var mailMessage = new MailMessage
+            {
+                From = new MailAddress(settings.CurrentValue.SenderEmail),
+                To = { message.To },
+                Subject = message.Subject,
+                Body = message.Body,
+                IsBodyHtml = true
+            };
+            mailMessage.Attachments.Add(message.PdfAttachment);
+            using var smtpClient = new SmtpClient(settings.CurrentValue.Host, settings.CurrentValue.Port)
+            {
+                Credentials = new System.Net.NetworkCredential(settings.CurrentValue.User, settings.CurrentValue.Password),
+                EnableSsl = true
+            };
+            await smtpClient.SendMailAsync(mailMessage);
+        }
+    }
+}
