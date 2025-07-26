27a4f3173d73c6d5448fb1ae56cfeae6061da6e3
AlaaHewwari <alaahewwari@gmail.com>
2025-07-26 03:11:29 +0300

    TABP-14 Add PdfService for generating PDFs from invoice HTML

---
+++ TABP/TABP.Infrastructure/Services/PdfService.cs
@@ -0,0 +1,17 @@
+﻿using TABP.Domain.Interfaces.Services;
+namespace TABP.Infrastructure.Services
+{
+    public class PdfService : IPdfService
+    {
+        public async Task<byte[]> GenerateBookingInvoicePdf(string Html)
+        {
+            IronPdf.License.LicenseKey = "IRONSUITE.ALAAHEWWARI.OUTLOOK.COM.3717-A28D764DF3-AV4OSEYZTPMS6GTB-UGW6PZ3AQHQ5-C2DVGSLFRZNT-2BGWNBOVUJE4-DU6FSL22JNFE-UGYFF2MJHBQ5-FV44MK-TYIEFYT6ODSQEA-DEPLOYMENT.TRIAL-DTXVLI.TRIAL.EXPIRES.24.AUG.2025";
+            var renderer = new ChromePdfRenderer();
+            var pdf = await renderer.RenderHtmlAsPdfAsync(Html);
+            pdf.SaveAs("html_saved.pdf");
+            var pdfBytes =await File.ReadAllBytesAsync("html_saved.pdf");
+            File.Delete("html_saved.pdf");
+            return pdfBytes;
+        }
+    }
+}
