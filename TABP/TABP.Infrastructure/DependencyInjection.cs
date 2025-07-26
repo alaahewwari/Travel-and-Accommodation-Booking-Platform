a66e0d74dd4aaba020f6b25b52a98e24e6961ca7
AlaaHewwari <alaahewwari@gmail.com>
2025-07-26 03:16:08 +0300

    TABP-14 Add registration for services using DI containers

--- TABP/TABP.Infrastructure/DependencyInjection.cs
+++ TABP/TABP.Infrastructure/DependencyInjection.cs
@@ -2,19 +2,30 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.IdentityModel.Tokens;
+using Sieve.Services;
 using System.Text;
 using TABP.Domain.Interfaces.Services;
 using TABP.Infrastructure.Common;
+using TABP.Infrastructure.Configurations;
 using TABP.Infrastructure.Services;
+using TABP.Infrastructure.Services.Email;
+using TABP.Infrastructure.Sieve;
 namespace TABP.Infrastructure
 {
     public static class DependencyInjection
     {
         public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
         {
+            services.AddScoped<SieveProcessor>();
+            services.AddScoped<SieveProcessor, HotelSieveProcessor>();
             services.AddScoped<IPasswordHasher, PasswordHasher>();
+            services.Configure<SmtpSettings>(
+            configuration.GetSection("Smtp"));
+            services.AddTransient<IEmailService, SmtpEmailService>();
             services.AddScoped<IJwtGenerator, JwtGenerator>();
-            services.Configure<JwtConfigurations>(configuration.GetSection("Jwt"));
+            services.AddTransient<IEmailMessageBuilder, EmailMessageBuilder>();
+            services.AddTransient<IPdfService,PdfService>();
+            services.AddTransient<IInvoiceTemplateBuilder, InvoiceTemplateBuilder>();
             services.AddAuthentication(options =>
             {
                 options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
@@ -24,7 +35,7 @@ namespace TABP.Infrastructure
             .AddJwtBearer(options =>
             {
                 var jwtConfig = configuration.GetSection("Jwt").Get<JwtConfigurations>();
-                var key = Encoding.UTF8.GetBytes(jwtConfig.Key);
+                var key = Encoding.UTF8.GetBytes(jwtConfig!.Key);
                 options.TokenValidationParameters = new TokenValidationParameters
                 {
                     ValidateIssuer = true,
