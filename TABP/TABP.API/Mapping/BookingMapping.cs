a688b60caeb508aa7dbf6b1a2fcc743edcae4578
AlaaHewwari <alaahewwari@gmail.com>
2025-07-26 03:24:32 +0300

    TABP-14 Add booking mapping class

---
+++ TABP/TABP.API/Mapping/BookingMapping.cs
@@ -0,0 +1,13 @@
+﻿using Riok.Mapperly.Abstractions;
+using TABP.API.Contracts.Bookings;
+using TABP.Application.Bookings.Commands.Create;
+using TABP.Application.Bookings.Queries.GetById;
+namespace TABP.API.Mapping
+{
+    [Mapper]
+    public static partial class BookingMapping
+    {
+        public static partial CreateBookingCommand ToCommand(this CreateBookingRequest request);
+        public static partial GetBookingsQuery ToQuery(this GetBookingsRequest request);
+    }
+}
