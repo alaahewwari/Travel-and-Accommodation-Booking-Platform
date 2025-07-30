d11b64ee4a2b8277d6f9b2a675099dbb5055b5ba
AlaaHewwari <alaahewwari@gmail.com>
2025-07-30 14:18:13 +0300

    TABP-16 Add mapping methods for amenities

---
+++ TABP/TABP.API/Mapping/AmenitiesMapping.cs
@@ -0,0 +1,16 @@
+﻿using Riok.Mapperly.Abstractions;
+using TABP.API.Contracts.Amenities;
+using TABP.Application.Amenities.Commands.Create;
+using TABP.Application.Amenities.Commands.Update;
+using TABP.Application.Amenities.Common;
+using TABP.Domain.Entities;
+namespace TABP.API.Mapping
+{
+    [Mapper]
+    public static partial class AmenitiesMapping
+    {
+        public static partial CreateAmenityCommand ToCommand(this CreateAmenityRequest request);
+        public static partial UpdateAmenityCommand ToCommand(this UpdateAmenityRequest request, long id);
+        public static partial AmenityResponse ToResponse(this Amenity amenity);
+    }
+}
