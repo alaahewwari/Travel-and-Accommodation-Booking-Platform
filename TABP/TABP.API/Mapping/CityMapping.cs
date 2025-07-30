248d4b3b125a5872b9470d946dc048a31260ce58
AlaaHewwari <alaahewwari@gmail.com>
2025-07-30 21:32:17 +0300

    TABP-17 Add image handling commands to mapping classes

--- TABP/TABP.API/Mapping/CityMapping.cs
+++ TABP/TABP.API/Mapping/CityMapping.cs
@@ -1,6 +1,8 @@
 ﻿using Riok.Mapperly.Abstractions;
 using TABP.API.Contracts.Cities;
+using TABP.API.Contracts.Images;
 using TABP.Application.Cities.Commands.Create;
+using TABP.Application.Cities.Commands.SetThumbnail;
 using TABP.Application.Cities.Commands.Update;
 namespace TABP.API.Mapping
 {
@@ -9,5 +11,6 @@ namespace TABP.API.Mapping
     {
         public static partial CreateCityCommand ToCommand(this CreateCityRequest request);
         public static partial UpdateCityCommand ToCommand(this UpdateCityRequest request, int id);
+        public static partial SetCityThumbnailCommand ToCityCommand(this SetImageRequest request, int cityId, Stream fileStream, string fileName);
     }
 }
--- TABP/TABP.API/Mapping/HotelMapping.cs
+++ TABP/TABP.API/Mapping/HotelMapping.cs
@@ -1,6 +1,9 @@
 ﻿using Riok.Mapperly.Abstractions;
 using TABP.API.Contracts.Hotels;
+using TABP.API.Contracts.Images;
+using TABP.Application.Hotels.Commands.AddToGallery;
 using TABP.Application.Hotels.Commands.Create;
+using TABP.Application.Hotels.Commands.SetThumbnail;
 using TABP.Application.Hotels.Commands.Update;
 using TABP.Application.Hotels.Queries.Search;
 namespace TABP.API.Mapping
@@ -11,5 +14,7 @@ namespace TABP.API.Mapping
         public static partial CreateHotelCommand ToCommand(this CreateHotelRequest request);
         public static partial UpdateHotelCommand ToCommand(this UpdateHotelRequest request, long id);
         public static partial SearchHotelsQuery ToQuery(this SearchHotelsRequest request);
+        public static partial AddImageToHotelGalleryCommand ToHotelGalleryCommand(this SetImageRequest request, long hotelId, Stream fileStream, string fileName);
+        public static partial SetHotelThumbnailCommand ToHotelThumbnailCommand(this SetImageRequest request, long hotelId, Stream fileStream, string fileName);
     }
 }
