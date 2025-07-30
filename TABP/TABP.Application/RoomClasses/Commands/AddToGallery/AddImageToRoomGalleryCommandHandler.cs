0ba729da479754d928d1054ce8e689ce0ab122fb
AlaaHewwari <alaahewwari@gmail.com>
2025-07-30 21:45:01 +0300

    TABP-17 Add image upload commands and handlers for room classes

---
+++ TABP/TABP.Application/RoomClasses/Commands/AddToGallery/AddImageToRoomGalleryCommandHandler.cs
@@ -0,0 +1,41 @@
+﻿using MediatR;
+using TABP.Application.Common;
+using TABP.Application.RoomClassClasses.Common;
+using TABP.Application.RoomClasses.Mapper;
+using TABP.Domain.Entities;
+using TABP.Domain.Enums;
+using TABP.Domain.Interfaces.Repositories;
+using TABP.Domain.Interfaces.Services;
+
+namespace TABP.Application.RoomClasses.Commands.AddToGallery
+{
+    public class AddImageToRoomGalleryCommandHandler(
+        IImageRepository<RoomImage> imageRepository,
+        IRoomClassRepository roomClassRepository,
+        ICloudinaryService cloudinaryService,
+        IUnitOfWork unitOfWork
+        ) : IRequestHandler<AddImageToRoomGalleryCommand, Result>
+    {
+        public async Task<Result> Handle(AddImageToRoomGalleryCommand request, CancellationToken cancellationToken)
+        {
+            var existingRoom = await roomClassRepository.GetRoomClassByIdAsync(request.RoomClassId, cancellationToken);
+            if (existingRoom is null)
+            {
+                return Result.Failure(RoomClassErrors.RoomClassNotFound);
+            }
+            var existingThumbnail = await imageRepository.ExistsAsync(request.RoomClassId, ImageType.Thumbnail, cancellationToken);
+            if (existingThumbnail)
+            {
+                await imageRepository.DeleteAsync(request.RoomClassId, ImageType.Thumbnail, cancellationToken);
+            }
+            string? imageUrl = null;
+            await unitOfWork.ExecuteResilientTransactionAsync(async cancellationToken =>
+            {
+                var imageUrl = await cloudinaryService.UploadImageAsync(request.FileStream, request.FileName, "rooms", cancellationToken);
+                var roomImage = request.ToRoomImageDomain(imageUrl, ImageType.Gallery);
+                await imageRepository.CreateAsync(roomImage, cancellationToken);
+            }, cancellationToken);
+            return Result<string>.Success(imageUrl!);
+        }
+    }
+}
--- TABP/TABP.Application/RoomClasses/Mapper/RoomClassMapper.cs
+++ TABP/TABP.Application/RoomClasses/Mapper/RoomClassMapper.cs
@@ -1,10 +1,13 @@
 ﻿using Riok.Mapperly.Abstractions;
 using System.Runtime.Serialization;
 using TABP.Application.Hotels.Common;
+using TABP.Application.RoomClasses.Commands.AddToGallery;
 using TABP.Application.RoomClasses.Commands.Create;
+using TABP.Application.RoomClasses.Commands.SetThumbnail;
 using TABP.Application.RoomClasses.Commands.Update;
 using TABP.Application.RoomClasses.Common;
 using TABP.Domain.Entities;
+using TABP.Domain.Enums;
 using TABP.Domain.Models.Hotel;
 namespace TABP.Application.RoomClasses.Mapper
 {
@@ -28,5 +31,25 @@ namespace TABP.Application.RoomClasses.Mapper
         public static partial FeaturedDealsHotelsResponse ToFeaturedDealsHotelsResponse(this FeaturedealsHotels roomClass);
         private static partial RoomClass ToRoomClassDomainInternal(CreateRoomClassCommand command);
         private static partial RoomClass ToRoomClassDomainInternal(UpdateRoomClassCommand command);
+        [MapperIgnoreSource(nameof(SetRoomThumbnailCommand.FileStream))]
+        [MapperIgnoreSource(nameof(SetRoomThumbnailCommand.FileName))]
+        private static partial RoomImage ToRoomImageDomainInternal(SetRoomThumbnailCommand command, string imageUrl);
+        public static RoomImage ToRoomImageDomain(this SetRoomThumbnailCommand command, string imageUrl, ImageType type)
+        {
+            var RoomImage = ToRoomImageDomainInternal(command, imageUrl);
+            RoomImage.CreatedAt = DateTime.UtcNow;
+            RoomImage.ImageType = type;
+            return RoomImage;
+        }
+        [MapperIgnoreSource(nameof(AddImageToRoomGalleryCommand.FileStream))]
+        [MapperIgnoreSource(nameof(AddImageToRoomGalleryCommand.FileName))]
+        private static partial RoomImage ToRoomImageDomainInternal(AddImageToRoomGalleryCommand command, string imageUrl);
+        public static RoomImage ToRoomImageDomain(this AddImageToRoomGalleryCommand command, string imageUrl, ImageType type)
+        {
+            var roomImage = ToRoomImageDomainInternal(command, imageUrl);
+            roomImage.CreatedAt = DateTime.UtcNow;
+            roomImage.ImageType = type;
+            return roomImage;
+        }
     }
 }
