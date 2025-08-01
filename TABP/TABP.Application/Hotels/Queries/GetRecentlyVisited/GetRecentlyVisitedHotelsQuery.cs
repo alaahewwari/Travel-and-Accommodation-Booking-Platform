f60caf71406771a0fbfa19b3ab7eec9ba586f2a0
AlaaHewwari <alaahewwari@gmail.com>
2025-08-01 17:14:45 +0300

    TABP-18 Improve error handling and refactor query handlers

--- TABP/TABP.Application/Hotels/Queries/GetRecentlyVisited/GetRecentlyVisitedHotelsQuery.cs
+++ TABP/TABP.Application/Hotels/Queries/GetRecentlyVisited/GetRecentlyVisitedHotelsQuery.cs
@@ -3,5 +3,5 @@ using TABP.Application.Common;
 using TABP.Application.Hotels.Common;
 namespace TABP.Application.Hotels.Queries.GetRecentlyVisited
 {
-    public record GetRecentlyVisitedHotelsQuery(int UserId, int Count) : IRequest<Result<List<HotelResponse>>>;
+    public record GetRecentlyVisitedHotelsQuery(long UserId, int Count) : IRequest<Result<IEnumerable<HotelResponse>>>;
 }
