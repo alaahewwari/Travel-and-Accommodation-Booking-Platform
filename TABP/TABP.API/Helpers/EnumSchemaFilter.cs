ddd1643627ab21ecae5f7f6da2122bf695e94673
AlaaHewwari <alaahewwari@gmail.com>
2025-08-01 18:57:31 +0300

    TABP-18 Add EnumSchemaFilter for enhanced enum support

---
+++ TABP/TABP.API/Helpers/EnumSchemaFilter.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace TABP.API.Helpers
+{
+    public class EnumSchemaFilter : ISchemaFilter
+    {
+        public void Apply(OpenApiSchema schema, SchemaFilterContext context)
+        {
+            if (context.Type.IsEnum)
+            {
+                var enumNames = Enum.GetNames(context.Type);
+                var enumValues = Enum.GetValues(context.Type).Cast<byte>();
+
+                var enumDescriptions = enumValues
+                    .Zip(enumNames, (value, name) => $"{value} = {name}");
+
+                schema.Description += " Possible values: " + string.Join(", ", enumDescriptions);
+            }
+        }
+    }
+}
