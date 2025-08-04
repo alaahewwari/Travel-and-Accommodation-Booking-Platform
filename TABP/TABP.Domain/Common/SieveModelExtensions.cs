add2638ef44aaef17b3ea8d62333c94f10fc7334
AlaaHewwari <alaahewwari@gmail.com>
2025-08-04 14:29:53 +0300

    TABP-23 Add SieveModelExtensions and update HotelSieveProcessor

---
+++ TABP/TABP.Domain/Common/SieveModelExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Sieve.Models;
+namespace TABP.Domain.Common
+{
+    public static class SieveModelExtensions
+    {
+        private const int DefaultPage = 1;
+        private const int DefaultPageSize = 10;
+        public static void EnsureDefaults(this SieveModel model)
+        {
+            model.Page ??= DefaultPage;
+            model.PageSize ??= DefaultPageSize;
+        }
+    }
+}
