4032ffae2e8cd9eedfe6af6c486078ccbe166cbf
AlaaHewwari <alaahewwari@gmail.com>
2025-08-04 22:58:49 +0300

    TABP-26 Add test infrastructure for integration testing

---
+++ TABP/Application.IntegrationTests/Common/TestInfrastructure/TestEmailService.cs
@@ -0,0 +1,17 @@
+﻿using TABP.Domain.Interfaces.Services;
+using TABP.Domain.Models.Email;
+namespace Application.IntegrationTests.Common.TestInfrastructure
+{
+    /// <summary>
+    /// Test implementation of email service that captures sent emails for verification in tests.
+    /// </summary>
+    public class TestEmailService : IEmailService
+    {
+        public List<EmailMessage> SentEmails { get; } = new();
+        public Task SendEmailAsync(EmailMessage emailMessage)
+        {
+            SentEmails.Add(emailMessage);
+            return Task.CompletedTask;
+        }
+    }
+}
