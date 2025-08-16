6d43a86d6536aa33d3f0a04ee397f3df69a0e3fe
AlaaHewwari <alaahewwari@gmail.com>
2025-08-16 12:11:46 +0300

    TABP-32 Change Rating data type from int to byte in Reviews

---
+++ TABP/TABP.Persistence/Migrations/20250815220724_ChangeRatingDataType.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TABP.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeRatingDataType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<byte>(
+                name: "Rating",
+                table: "Reviews",
+                type: "tinyint",
+                nullable: false,
+                defaultValue: (byte)0,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Rating",
+                table: "Reviews",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(byte),
+                oldType: "tinyint",
+                oldDefaultValue: (byte)0);
+        }
+    }
+}
